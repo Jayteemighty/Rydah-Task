e8205005e4b9688b9a8ad620236818b243de78b5
Adesina Joshua <tolujosh1@gmail.com>
2024-03-18 11:40:58 +0100

    feature:new requirements.txt

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 
 urlpatterns = [
-    #path('register/', views.RegisterAPIView.as_view()),
-    #path('login/', views.LoginAPIView.as_view()),
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
 ]
